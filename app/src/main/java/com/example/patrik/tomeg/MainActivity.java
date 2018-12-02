6f0c329729404b9536fc22da3e32557ca93b07f1
Kacsa22 <kovacs.patrik@hotmail.com>
2018-12-02 19:58:31 +0100

    Számolás megcsinálva

--- app/src/main/java/com/example/patrik/tomeg/MainActivity.java
+++ app/src/main/java/com/example/patrik/tomeg/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.patrik.tomeg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button szamol_button;
+    EditText atmero_edit,hossz_edit,eredmeny_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        szamol_button = findViewById(R.id.szamol_button);
+        atmero_edit = findViewById(R.id.atmero_edit);
+        hossz_edit = findViewById(R.id.hossz_edit);
+        eredmeny_edit = findViewById(R.id.eredmeny_edit);
+        szamol_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double atmero,hossz;
+                try {
+                    atmero = Double.parseDouble(atmero_edit.getText().toString());
+                    hossz = Double.parseDouble(hossz_edit.getText().toString());
+                    szamol(atmero,hossz);
+                }
+                catch(Exception e){
+                    Toast.makeText(MainActivity.this,"Hülye vagy fiam!!!",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
+    private void szamol(Double atmero, Double hossz){
+        Double eredmeny;
+
+        eredmeny = (((((atmero*atmero)*Math.PI)/4)*hossz/1000000)*7.8);
+        eredmeny_edit.setText(String.valueOf(eredmeny));
+    }
+
 }
