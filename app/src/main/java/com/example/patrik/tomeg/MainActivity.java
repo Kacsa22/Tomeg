cae6b9e4afe28fa9668902ae8cb2e87ecee4fdb2
Kacsa22 <kovacs.patrik@hotmail.com>
2018-12-03 20:29:22 +0100

    anyag kiválasztás gui

--- app/src/main/java/com/example/patrik/tomeg/MainActivity.java
+++ app/src/main/java/com/example/patrik/tomeg/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.patrik.tomeg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button szamol_button;
     EditText atmero_edit,hossz_edit,eredmeny_edit;
+    Double suruseg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
         atmero_edit = findViewById(R.id.atmero_edit);
         hossz_edit = findViewById(R.id.hossz_edit);
         eredmeny_edit = findViewById(R.id.eredmeny_edit);
+        Spinner spinner =findViewById(R.id.anyag_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.anyag_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        Toast.makeText(MainActivity.this,String.valueOf(spinner.getSelectedItemId()),Toast.LENGTH_LONG);
         szamol_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     hossz = Double.parseDouble(hossz_edit.getText().toString());
                     szamol(atmero,hossz);
                 }
+
                 catch(Exception e){
                     Toast.makeText(MainActivity.this,"Hülye vagy fiam!!!",Toast.LENGTH_LONG).show();
                 }
@@ -39,8 +49,17 @@ public class MainActivity extends AppCompatActivity {
     private void szamol(Double atmero, Double hossz){
         Double eredmeny;
 
-        eredmeny = (((((atmero*atmero)*Math.PI)/4)*hossz/1000000)*7.8);
+        eredmeny = (((((atmero*atmero)*Math.PI)/4)*hossz/1000000)*suruseg);
         eredmeny_edit.setText(String.valueOf(eredmeny));
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
