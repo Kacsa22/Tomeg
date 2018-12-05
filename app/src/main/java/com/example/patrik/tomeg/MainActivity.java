15189ae7d723b428afca8fe0b04744dcd290776e
Kacsa22 <kovacs.patrik@hotmail.com>
2018-12-05 21:34:47 +0100

    anyag kiválasztás fejlesztve

--- app/src/main/java/com/example/patrik/tomeg/MainActivity.java
+++ app/src/main/java/com/example/patrik/tomeg/MainActivity.java
@@ -10,23 +10,33 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button szamol_button;
     EditText atmero_edit,hossz_edit,eredmeny_edit;
     Double suruseg;
+    ArrayList<Anyag> anyagok = new ArrayList<>();
+    Spinner spinner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        anyagok.add(new Anyag("Vas",7.87));
+        anyagok.add(new Anyag("Bronz",8.2));
+        //TODO: Objektumokat XML fájlból
         szamol_button = findViewById(R.id.szamol_button);
         atmero_edit = findViewById(R.id.atmero_edit);
         hossz_edit = findViewById(R.id.hossz_edit);
         eredmeny_edit = findViewById(R.id.eredmeny_edit);
-        Spinner spinner =findViewById(R.id.anyag_spinner);
+        spinner = findViewById(R.id.anyag_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.anyag_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // TODO: spinner cuccait osztályból feltölteni
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
         szamol_button.setOnClickListener(new View.OnClickListener() {
@@ -48,23 +58,32 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private void szamol(Double atmero, Double hossz){
         Double eredmeny;
-
+        suruseg = anyagok.get(spinner.getSelectedItemPosition()).getSuruseg();
         eredmeny = (((((atmero*atmero)*Math.PI)/4)*hossz/1000000)*suruseg);
         eredmeny_edit.setText(String.valueOf(eredmeny));
     }
 
     class Anyag{
+        Integer sorszam;
         String nev;
         Double suruseg;
         public Anyag(String nev, Double suruseg){
             this.nev = nev;
             this.suruseg = suruseg;
         }
+
+        public String getNev(){
+            return nev;
+        }
+
+        public double getSuruseg(){
+            return suruseg;
+        }
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        
+
     }
 
     @Override
