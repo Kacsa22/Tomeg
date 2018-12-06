6512b5e90f9060be9e092df52d273a83f2816b67
Kacsa22 <kovacs.patrik@hotmail.com>
2018-12-06 20:08:31 +0100

    Spinner feltöltése osztályból

--- app/src/main/java/com/example/patrik/tomeg/MainActivity.java
+++ app/src/main/java/com/example/patrik/tomeg/MainActivity.java
@@ -7,76 +7,85 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.DOMStringList;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button szamol_button;
-    EditText atmero_edit,hossz_edit,eredmeny_edit;
+    EditText atmero_edit, hossz_edit, eredmeny_edit;
     Double suruseg;
     ArrayList<Anyag> anyagok = new ArrayList<>();
     Spinner spinner;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        anyagok.add(new Anyag("Vas",7.87));
-        anyagok.add(new Anyag("Bronz",8.2));
+        anyagok.add(new Anyag("Vas", 7.87));
+        anyagok.add(new Anyag("Bronz", 8.2));
+        anyagok.add(new Anyag("Aluminium", 2.7));
         //TODO: Objektumokat XML fájlból
         szamol_button = findViewById(R.id.szamol_button);
         atmero_edit = findViewById(R.id.atmero_edit);
         hossz_edit = findViewById(R.id.hossz_edit);
         eredmeny_edit = findViewById(R.id.eredmeny_edit);
         spinner = findViewById(R.id.anyag_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.anyag_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // TODO: spinner cuccait osztályból feltölteni
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, Nevlist());
         spinner.setAdapter(adapter);
         szamol_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double atmero,hossz;
+                double atmero, hossz;
                 try {
                     atmero = Double.parseDouble(atmero_edit.getText().toString());
                     hossz = Double.parseDouble(hossz_edit.getText().toString());
-                    szamol(atmero,hossz);
-                }
-
-                catch(Exception e){
-                    Toast.makeText(MainActivity.this,"Hülye vagy fiam!!!",Toast.LENGTH_LONG).show();
+                    szamol(atmero, hossz);
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Hülye vagy fiam!!!", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
 
-    private void szamol(Double atmero, Double hossz){
+    private void szamol(Double atmero, Double hossz) {
         Double eredmeny;
         suruseg = anyagok.get(spinner.getSelectedItemPosition()).getSuruseg();
-        eredmeny = (((((atmero*atmero)*Math.PI)/4)*hossz/1000000)*suruseg);
+        eredmeny = (((((atmero * atmero) * Math.PI) / 4) * hossz / 1000000) * suruseg);
         eredmeny_edit.setText(String.valueOf(eredmeny));
     }
 
-    class Anyag{
-        Integer sorszam;
+    class Anyag {
         String nev;
         Double suruseg;
-        public Anyag(String nev, Double suruseg){
+
+        public Anyag(String nev, Double suruseg) {
             this.nev = nev;
             this.suruseg = suruseg;
         }
 
-        public String getNev(){
+        public String getNev() {
             return nev;
         }
 
-        public double getSuruseg(){
+        public double getSuruseg() {
             return suruseg;
         }
+
+
+    }
+
+    public ArrayList<String> Nevlist() {
+        ArrayList<String> nevlist = new ArrayList<>();
+        for (int i = 0; i < anyagok.size(); i++) {
+            nevlist.add(anyagok.get(i).getNev());
+        }
+        return nevlist;
     }
 
 }
